c28c3a7705157b35a3f13e40d96aa53db641a33c
Robert Anderson <robert.anderson.fl@gmail.com>
2020-10-07 23:08:00 -0400

    Work in progress on the omnibar features of the extension

--- src/app/background.js
+++ src/app/background.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import { runtime } from 'utils/Runtime.js'
 
 class Background {
@@ -5,10 +7,24 @@ class Background {
 		const omnibox = runtime.api('omnibox');
 		const tabs = runtime.api('tabs');
 
+		// omnibox.onInputEntered.addListener(function(text) {
+		// 	// Encode user input for special characters , / ? : @ & = + $ #
+		// 	var newURL = 'https://www.google.com/search?q=' + encodeURIComponent(text);
+		// 	tabs.create({ url: newURL });
+		// });
+
+		// This event is fired each time the user updates the text in the omnibox,
+		// as long as the extension's keyword mode is still active.
+		omnibox.onInputChanged.addListener(function(text, suggest) {			
+			suggest([
+				{content: text + " one", description: "the first one"},
+				{content: text + " number two", description: "the second entry"}
+			]);
+		});
+
+		// This event is fired with the user accepts the input in the omnibox.
 		omnibox.onInputEntered.addListener(function(text) {
-			// Encode user input for special characters , / ? : @ & = + $ #
-			var newURL = 'https://www.google.com/search?q=' + encodeURIComponent(text);
-			tabs.create({ url: newURL });
+			alert('You just typed "' + text + '"');
 		});
 	}
 }

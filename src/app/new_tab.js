11fd4c902ce5e2c4134a25d87451574b46005536
Robert Anderson <robert.anderson.fl@gmail.com>
2025-08-24 22:15:36 -0400

    Fixes for manifest v3

--- src/app/new_tab.js
+++ src/app/new_tab.js
@@ -1,11 +1,1 @@
-// Having issues locally but it works when loaded unpacked in the browser
-
-document.addEventListener('click', function (event) {
-	window.location.href = 'https://feathermarks.com/home';
-}, false);
-
-var subtitle = document.getElementById("subtitle"); 
-
-subtitle.addEventListener('keydown', function (event) {
-	window.location.href = 'https://feathermarks.com/home';
-}, false);
+window.location.replace("https://feathermarks.com/home");

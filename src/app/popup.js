614a83463e2a9b19dd4bd4087480c6ede2e84019
Robert Anderson <robert.anderson.fl@gmail.com>
2025-08-25 22:36:41 -0400

    More proof of concept code, this time to grab the url of the current page

--- src/app/popup.js
+++ src/app/popup.js
@@ -1,17 +1,19 @@
-// popup.js
-
 document.addEventListener('DOMContentLoaded', () => {
-  // Query for the current active tab.
-  // The 'activeTab' permission allows us to do this.
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    // We expect the query to return an array with a single tab.
     const current_tab = tabs[0];
     const tab_title_element = document.getElementById('tab_title');
+    let new_text = '';
     
     if (current_tab && current_tab.title) {
-      tab_title_element.textContent = current_tab.title;
+      new_text = current_tab.title;
     } else {
-      tab_title_element.textContent = 'Could not get tab title.';
+      new_text = 'Could not get tab title.';
     }
+
+    if (current_tab && current_tab.url) {
+      new_text += ' and url = ' + current_tab.url;
+    }
+
+    tab_title_element.textContent = new_text;
   });
 });
